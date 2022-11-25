ff69cbdaf9eb7e2c3e1c163308fdf24cd60814a7
Shawn <lcorel86@gmail.com>
2022-11-25 01:21:11 -0600

    its not always good to add type everywhere

--- 01intro/variableMe.js
+++ 01intro/variableMe.js
@@ -1,2 +1,11 @@
+"use strict";
+exports.__esModule = true;
 var greetings = "Hello Shawn";
+var myNum = 6;
+greetings.toLowerCase();
 console.log(greetings);
+// number
+var userId = 334455;
+userId.toFixed();
+// boolean
+var isLoggedIn = false;
--- 01intro/variableMe.ts
+++ 01intro/variableMe.ts
@@ -6,5 +6,12 @@ greetings.toLowerCase()
 
 console.log(greetings);
 
+// number
+let userId = 334455;
+
+userId.toFixed()
+// boolean
+let isLoggedIn: boolean = false;
+
 
 export {}
