01b2a9d1e26c8117c0e684fbf8e27abc280c34e5
Shawn <lcorel86@gmail.com>
2022-11-27 02:12:31 -0600

    finished array part

--- 02Basics/myObjects.js
+++ 02Basics/myObjects.js
@@ -1,9 +1,35 @@
+"use strict";
+exports.__esModule = true;
 var User = {
     name: "shawn",
     email: "shawn@gmail.com",
     isAvtive: true
 };
-function createUser(_a) {
-    var string = _a.name, boolean = _a.isPaid;
+// function createUser({name: string, isPaid: boolean}) {
+// }
+var newUser = { name: "shawn", isPaid: false, email: "shawn@gmail.com" };
+// createUser(newUser)
+function createCourse() {
+    return { name: "shawn", price: 15 };
 }
-createUser();
+var myUser = {
+    _id: "1234",
+    name: "h",
+    email: "h@h.com",
+    isActive: false
+};
+myUser.email = "h@gmail.com";
+
+
+// type cardNumber = {
+//     cardnumber: string
+// }
+
+// type cardDate = {
+//     cardDate: string
+// }
+
+
+// type cardDetails = cardNumber & cardDate & {
+//     cvv: number
+// }
--- 02Basics/myObjects.ts
+++ 02Basics/myObjects.ts
@@ -5,17 +5,54 @@ const User = {
 }
 
 
-function createUser({name: string, isPaid: boolean}) {
+// function createUser({name: string, isPaid: boolean}) {
 
-}
+// }
 
 const newUser = {name: "shawn", isPaid: false, email: "shawn@gmail.com"}
 
-createUser(newUser)
+// createUser(newUser)
 
 function createCourse():{name:string, price:number}{
     return {name:"shawn", price: 15}
 }
+
+
+// type User = {
+//     name: string,
+//     email: string,
+//     isActive: boolean
+// }
+
+// type Mystring = string
+
+// function createUser(user: User): User{
+//     return {name: "", email: "", isActive: true}
+// }
+
+// createUser({name: "", email: "", isActive: true})
+
+
+type User = {
+    readonly _id: string,
+    name: string,
+    email: string,
+    isActive: boolean,
+    credcardDetails?: number
+}
+
+
+let myUser: User = {
+    _id: "1234",
+    name: "h",
+    email: "h@h.com",
+    isActive: false
+}
+
+myUser.email = "h@gmail.com"
+
+
+
 
 
 
